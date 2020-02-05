c7da5a48851f42d5c0e9d5cbb457affde2afc8f4
Alex Perkowski <perkowskialex@gmail.com>
2020-02-05 16:24:21 -0600

    add directory and menu item components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import HomePage from "../src/components/HomePage/HomePage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./HomePage.scss";
+import Directory from "../../components/Directory/Directory";
+
+const HomePage = () => {
+  return <Directory />;
+};
+
+export default HomePage;
