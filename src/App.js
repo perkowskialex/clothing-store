92f4898b770d0b9969d8e07847e778a26a0c8599
Alex Perkowski <perkowskialex@gmail.com>
2020-02-07 14:42:14 -0600

    user subscription on state change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
 import ShopPage from "./pages/ShopPage/ShopPage";
 import SignInPage from "./pages/SignInPage/SignInPage";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route component={HomePage} exact path="/" />{" "}
-        <Route component={ShopPage} exact path="/shop" />{" "}
-        <Route component={SignInPage} exact path="/signin" />{" "}
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    //open subscription, calls user on state change
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // close subscription
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route component={HomePage} exact path="/" />{" "}
+          <Route component={ShopPage} exact path="/shop" />{" "}
+          <Route component={SignInPage} exact path="/signin" />{" "}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
