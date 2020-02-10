fe94de6b6ecb745464e5dbce3e693bf1e05dd4d3
Alex Perkowski <perkowskialex@gmail.com>
2020-02-10 16:54:57 -0600

    styling for add to cart

--- src/components/CollectionItem/CollectionItem.jsx
+++ src/components/CollectionItem/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../CustomButton/CustomButton";
 import "./CollectionItem.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
@@ -9,6 +10,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>ADD TO CART</CustomButton>
     </div>
   );
 };
