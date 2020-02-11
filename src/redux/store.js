275f61c225d3133e857369c2c79faf804e25ef1f
Alex Perkowski <perkowskialex@gmail.com>
2020-02-11 17:06:49 -0600

    local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -6,5 +7,6 @@ import rootReducer from "./root-reducer";
 const middlewares = [logger];
 
 //spread in all values from logger array, can add it to array value if needed
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
-export default store;
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
+export default { store, persistor };
