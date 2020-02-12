b176e9233766ca55a9ee1e2b97a294a8e19f4a31
Alex Perkowski <perkowskialex@gmail.com>
2020-02-12 13:34:17 -0600

    set up collection pages

---
+++ src/pages/Collection/Collection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Collection.scss";
+
+import CollectionItem from "../../components/CollectionItem/CollectionItem";
+
+const CollectionPage = () => {
+  return (
+    <div className="collection-page">
+      <h2>CATEGORY PAGE</h2>
+    </div>
+  );
+};
+
+export default CollectionPage;
--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,22 +1,15 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { Route } from "react-router-dom";
+import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
+import CategoryPage from "../Collection/Collection";
 
-import PreviewCollection from "../../components/PreviewCollection/PreviewCollection";
-import { selectCollections } from "../../redux/shop/shop-selectors";
-
-const ShopPage = ({ collections }) => {
+const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <PreviewCollection key={id} {...otherCollectionProps} />
-      ))}{" "}
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CategoryPage} />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
