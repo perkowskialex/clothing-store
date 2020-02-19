95c812e7f5dc5ad34b6e89d493d3c7ba448dd869
Alex Perkowski <perkowskialex@gmail.com>
2020-02-19 16:47:58 -0600

    add spinner animation to collections

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
+
+import WithSpinner from "../../components/WithSpinner/WithSpinner";
 import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
 import CollectionPage from "../Collection/Collection";
 import {
@@ -11,7 +13,12 @@ import { updateCollections } from "../../redux/shop/shop-actions";
 
 import { connect } from "react-redux";
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = { loading: true };
+
   unsubscribeFromSnapshot = null;
   componentDidMount() {
     const { updateCollections } = this.props;
@@ -20,17 +27,27 @@ class ShopPage extends React.Component {
     this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      this.setState({ loading: false });
     });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
