e31f50bd2f1f245f9290efdb9c56ddfd6eebf214
Alex Perkowski <perkowskialex@gmail.com>
2020-02-26 17:18:10 -0600

    set up stripe front end payment post, need to fix public key for stripe

--- client/src/components/StripeButton/StripeButton.jsx
+++ client/src/components/StripeButton/StripeButton.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_ojiXUrXlBInXN9zuzOR4ptmq00sajiezEA";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment Succesful");
+    // pass info to axios
+    axios({
+      url: "payment",
+      // post request
+      method: "post",
+      // pass token and amount for back end
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+      // check out response
+    })
+      .then(response => {
+        // if successful, pop up
+        alert("payment successful");
+      })
+      //
+      .catch(error => {
+        console.log("payment error", JSON.parse(error));
+        alert(
+          "There was an issue with your payment. Please use the test credit card"
+        );
+      });
   };
 
   return (
